de690f105aa18d3a1c0c0d308fcc9467e0972bad
rachel <jrachelr1@gmail.com>
2022-11-15 08:40:12 -0800

    added hover effect

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -11,27 +11,30 @@ export default class Porfolio extends Component {
           <div className="twelve columns collapsed">
             <h1>Explore my work</h1>
             <div className="portfolio-container">
-              <div className="portfolio-item">
-                <h4>LetsPlayPiano</h4>
-                <a href="https://gitlab.com/jrachelr1/piano">
+              <a href="https://gitlab.com/jrachelr1/piano" className="portfolio-item">
+                <div className="overlay">
+                  <h4>LetsPlayPiano</h4>
                   <img src="images/piano.jpg" alt="close up of a piano keyboard"></img>
-                </a>
-                <p>An interactive piano built using React Hooks and the Soundfont Player library.</p>
-              </div>
-              <div className="portfolio-item">
-                <h4>CarCar</h4>
-                <a href="https://gitlab.com/jrachelr1/car-car">
+                  <p>An interactive piano built using React Hooks and the Soundfont Player library.</p>
+                </div>
+              </a>
+              <a href="https://gitlab.com/jrachelr1/car-car" className="portfolio-item">
+                <div className="overlay">
+                  <h4>CarCar</h4>
+
                   <img src="images/car.png" alt="side of car"></img>
-                </a>
-                <p>A full-stack microservice application to manage a car dealership and service department built with Django and React.</p>
-              </div>
-              <div className="portfolio-item">
-                <h4>ConferenceGO</h4>
-                <a href="https://gitlab.com/jrachelr1/fearless-frontend">
+                  <p>A full-stack microservice application to manage a car dealership and service department built with Django and React.</p>
+                </div>
+              </a>
+              <a href="https://gitlab.com/jrachelr1/fearless-frontend" className="portfolio-item">
+                <div className="overlay">
+                  <h4>ConferenceGO</h4>
+
                   <img src="images/conference.jpg" alt="windows at a conference"></img>
-                </a>
-                <p>A conference management application implementing third party APIs</p>
-              </div>
+
+                  <p>A conference management application implementing third party APIs</p>
+                </div>
+              </a>
             </div>
           </div>
         </div>
